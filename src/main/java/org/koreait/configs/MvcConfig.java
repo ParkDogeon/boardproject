e4e26c35016d8dccf797ebf0e49f8fe7decb7e02
ParkDogeon <bdg3126@gmail.com>
2023-05-20 17:05:52 +0900

    템플릿 레이아웃 프론트 완료

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -1,17 +1,28 @@
 package org.koreait.configs;
 
-import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+
 
     @Value("${file.upload.path}")
     private String fileUploadPath;
+
+    // 사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
+
+
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -19,9 +30,20 @@ public class MvcConfig implements WebMvcConfigurer {
                 .setViewName("main/index");
     }
 
+
     @Override // application.properties 에 있는 파일 업로드 부분과 연동
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + fileUploadPath);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
+    }
 }
+
+
+
